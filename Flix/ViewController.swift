2806722c6591e131c37f8b491178f2ad008f7696
Stephanie Le <tle3@macalester.edu>
2019-10-20 19:42:05 -0500

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Stephanie Le on 10/20/19.
+//  Copyright © 2019 codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
