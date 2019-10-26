475e83116b72707ecae70b99b56e12dbd86c8a3f
Stephanie Le <tle3@macalester.edu>
2019-10-25 21:48:58 -0500

    Finished part 2

---
+++ Flix/MovieDetailViewController.swift
@@ -0,0 +1,56 @@
+//
+//  MovieDetailViewController.swift
+//  Flix
+//
+//  Created by Stephanie Le on 10/25/19.
+//  Copyright © 2019 codepath. All rights reserved.
+//
+
+import UIKit
+import AlamofireImage
+
+class MovieDetailViewController: UIViewController {
+    
+    @IBOutlet weak var backdropView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var sypnosisLabel: UILabel!
+    
+    var movie: [String:Any]!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        titleLabel.text = movie["title"] as? String
+        titleLabel.sizeToFit()
+        
+        sypnosisLabel.text = movie["overview"] as? String
+        sypnosisLabel.sizeToFit()
+        
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)!
+        
+        posterView.af_setImage(withURL: posterUrl)
+        
+        let backdropPath = movie["backdrop_path"] as! String
+        let backdropUrl = URL(string: "https://image.tmdb.org/t/p/w780" + backdropPath)!
+        
+        backdropView.af_setImage(withURL: backdropUrl)
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Stephanie Le on 10/25/19.
+//  Copyright © 2019 codepath. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
